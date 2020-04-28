aa15839e2d4be7b8b715fa193ac7f1013d326528
sunh423 <64500766+sunh423@users.noreply.github.com>
2020-04-28 15:57:26 -0700

    Update solution.py

--- 5kyu - What's a Perfect Power anyway?/solution.py
+++ 5kyu - What's a Perfect Power anyway?/solution.py
@@ -1,1 +1,13 @@
-
+#https://www.codewars.com/kata/54d4c8b08776e4ad92000835/train/python
+def isPP(n):
+    solution = []
+    for i in range(2,n):
+        sq_i = i
+        power = 1
+        while sq_i < n:
+            sq_i *= i
+            power += 1
+            if sq_i == n:
+                return [i, power]
+    else:
+        return None
