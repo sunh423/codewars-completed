11c2ac5297f0606568982bcae1b3bd9dfcc0ad41
sunh423 <64500766+sunh423@users.noreply.github.com>
2020-04-28 14:57:04 -0700

    Update solution.py

--- 6kyu - Who Likes It/solution.py
+++ 6kyu - Who Likes It/solution.py
@@ -1,1 +1,13 @@
-
+def likes(names):
+    like_string = ""
+    if len(names) == 0:
+        like_string = 'no one likes this'
+    elif len(names) == 1:
+        like_string = f'{names[0]} likes this'
+    elif len(names) == 2:
+        like_string = f'{names[0]} and {names[1]} like this'
+    elif len(names) == 3:
+        like_string = f'{names[0]}, {names[1]} and {names[2]} like this'
+    elif len(names) >= 4:
+        like_string = f'{names[0]}, ' + f'{names[1]} and ' + str(len(names) - 2) + ' others like this'
+    return like_string
