bc009281bf5d97064e1ff2a67b7b68e19bf64188
sunh423 <sunh423@gmail.com>
2022-06-03 01:24:39 -0700

    Uploaded javascript solution

---
+++ 8kyu - Function 1 - hello wrold/solution.js
@@ -0,0 +1,2 @@
+// https://www.codewars.com/kata/523b4ff7adca849afe000035/train/javascript
+const greet = () => "hello world!" //Arrow notation to create anonymous function, then save it to the variable greet.
